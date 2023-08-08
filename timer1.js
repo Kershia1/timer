b0ac046dad1d862f71c2fe67207bb412267779e0
Kershia1 <insatiable.meerkat@gmail.com>
2023-08-07 18:42:28 -0700

    timer1.js

--- timer1.js
+++ timer1.js
@@ -10,19 +10,20 @@ test command line input: node timer1.js 10 3 5 15 9
 process.stdout.write('\x07'); // makes a beep sound ?!?
 
 function alarmBeep(beeps) {
-  let delay = 1000; // delay is 1 second
+  const delayedBeep = delay => {// delay is 1 second
   for (let i = 0; i < beeps.length; i++) { // loop through the array
     if (beeps[i] > 0) { // if the element is greater than 0 @ index i
       setTimeout(() => { // set a timeout
         process.stdout.write('\x07'); //standard out beep sound,beep to console
-      }, delay += beeps[i]); // delay is 1 second + the element
-    } else { (beeps[i] < 0 || isNaN(beeps[i])) // 3 edge cases
-      // clearInterval(intervalId); // Stop the interval don't need this
-      console.log(`these are not the beeps your looking for: ${beeps}`); // print this message
+      }, delay * 1000); // delay is 1 second * the second amount
+      delay += beeps[i]; // increment 
+    } else if (beeps[i] < 0 || isNaN(beeps[i])) { // 3 edge cases
+      console.log(`these are not the beeps your looking for: ${beeps}`); 
     } // if the element is less than 0
-    console.log('beep'); // print beep
   }
-},1000);
+ };
+ delayedBeep(1); // call the function
+}
 
 // setInterval(() => {
 //   process.stdout.write('\x07');
