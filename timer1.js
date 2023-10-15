e7fd01eacdd4867d1bcb4b1ae54fe42e6671db70
Kershia1 <insatiable.meerkat@gmail.com>
2023-10-14 22:00:53 -0700

    odified

--- timer1.js
+++ timer1.js
@@ -1,10 +1,5 @@
 /*
-create a alarm clock timer, beeps at specified times 
-
-3,5,9,10,15 seconds
-
-test command line input: node timer1.js 10 3 5 15 9
-
+create a alarm clock timer, which beeps at specified times when a CLI is recieved.
 */
 
 process.stdout.write('\x07'); // makes a beep sound ?!?
@@ -19,18 +14,13 @@ function alarmBeep(beeps) {
       delay += beeps[i]; // increment 
     } else if (beeps[i] < 0 || isNaN(beeps[i])) { // 3 edge cases
       console.log(`these are not the beeps your looking for: ${beeps}`); 
-    } // if the element is less than 0
+    }
   }
  };
  delayedBeep(1); // call the function
 }
 
-// setInterval(() => {
-//   process.stdout.write('\x07');
-// });
-//beep array
-alarmBeep([10,"hello",, 3, 5, 15, 9]);
-//Edge cases
-// if no numbers are provided: no beeps 
-// if an input is a negative number: ignore/skip it
-// if an input is not a number: ignore/skip it
+//Edge Cases to test 
+alarmBeep([10, 3, 5, 15, -9]); // neg integer => skip 
+alarmBeep([ , ]); // no nums => no beeps 
+alarmBeep(["hello"]); // NaN => skip 
